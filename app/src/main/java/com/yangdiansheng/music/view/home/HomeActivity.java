9bfdb8b28dbce1fcbdf23f49f2ac00a67ab9e34c
杨殿生 <893187229@qq.com>
2019-11-09 22:42:27 +0800

    首页框架搭建

---
+++ app/src/main/java/com/yangdiansheng/music/view/home/HomeActivity.java
@@ -0,0 +1,109 @@
+package com.yangdiansheng.music.view.home;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+
+import com.yangdiansheng.lib_common_ui.ScaleTransitionPagerTitleView;
+import com.yangdiansheng.music.R;
+import com.yangdiansheng.music.view.home.adapter.HomePagerAdapter;
+import com.yangdiansheng.music.view.home.model.CHANNEL;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
+public class HomeActivity extends FragmentActivity implements View.OnClickListener{
+
+    private static final CHANNEL[] CHANNELS =
+            new CHANNEL[]{CHANNEL.MY, CHANNEL.DISCORY, CHANNEL.FRIEND};
+
+    /*
+     * View
+     */
+    private DrawerLayout mDrawerLayout;
+    private View mToggleView;
+    private View mSearchView;
+    private ViewPager mViewPager;
+    private HomePagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggleView = findViewById(R.id.toggle_view);
+        mToggleView.setOnClickListener(this);
+        mSearchView = findViewById(R.id.search_view);
+        mViewPager = findViewById(R.id.view_pager);
+        mAdapter = new HomePagerAdapter(getSupportFragmentManager(), CHANNELS);
+        mViewPager.setAdapter(mAdapter);
+        initMagicIndicator();
+    }
+
+    //初始化指示器
+    private void initMagicIndicator() {
+        MagicIndicator magicIndicator = findViewById(R.id.magic_indicator);
+        magicIndicator.setBackgroundColor(Color.WHITE);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return CHANNELS == null ? 0 : CHANNELS.length;
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                SimplePagerTitleView simplePagerTitleView = new ScaleTransitionPagerTitleView(context);
+                simplePagerTitleView.setText(CHANNELS[index].getKey());
+                simplePagerTitleView.setTextSize(19);
+                simplePagerTitleView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                simplePagerTitleView.setNormalColor(Color.parseColor("#999999"));
+                simplePagerTitleView.setSelectedColor(Color.parseColor("#333333"));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                return null;
+            }
+
+            @Override
+            public float getTitleWeight(Context context, int index) {
+                return 1.0f;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/yangdiansheng/music/view/home/adapter/HomePagerAdapter.java
@@ -0,0 +1,43 @@
+package com.yangdiansheng.music.view.home.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.yangdiansheng.music.view.discory.DiscoryFragment;
+import com.yangdiansheng.music.view.friend.FriendFragment;
+import com.yangdiansheng.music.view.home.model.CHANNEL;
+import com.yangdiansheng.music.view.mine.MineFragment;
+
+
+/**
+ * 首页ViewPger的Adpater
+ */
+public class HomePagerAdapter extends FragmentPagerAdapter {
+    private CHANNEL[] mList;
+
+    public HomePagerAdapter(FragmentManager fm, CHANNEL[] datas) {
+        super(fm);
+        mList = datas;
+    }
+
+    //初始化对应的fragment
+    @Override
+    public Fragment getItem(int position) {
+        int type = mList[position].getValue();
+        switch (type) {
+            case CHANNEL.MINE_ID:
+                return MineFragment.newInstance();
+            case CHANNEL.DISCORY_ID:
+                return DiscoryFragment.newInstance();
+            case CHANNEL.FRIEND_ID:
+                return FriendFragment.newInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return mList == null ? 0 : mList.length;
+    }
+}
